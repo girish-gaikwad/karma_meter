33327783138a599ff0fc105641ea0bf8b5d23821
MukeshR-prog <mukeshr1855@gmail.com>
2024-09-27 19:02:45 +0530

    Updated UI

--- frontend/src/components/mukesh/donutgraph.jsx
+++ frontend/src/components/mukesh/donutgraph.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
-import { Box, Typography } from "@mui/material";
-import "./donutgraph.css"; // Your custom CSS file
-import arrow from '../../assets/arrow.png';
+import "./donutgraph.css";
+import arrow from "../../assets/arrow.png";
 Chart.register(ArcElement, Tooltip, Legend);
 
 const CarbonFootprintChart = () => {
   const data = {
-    labels: ["Commute", "Food", "Appliances"], // Labels still exist in the data, but won't be shown
+    labels: ["Commute", "Food", "Appliances"],
     datasets: [
       {
         data: [40, 30, 30],
-        backgroundColor: ["#FF8B1F", "#FF5E58", "#00C48C"],
+        backgroundColor: ["#F58E27", "#F4646E", "#4AC596"],
         hoverBackgroundColor: ["#FF8B1F", "#FF5E58", "#00C48C"],
         borderWidth: 0,
-        
-        
-        cutout: "90%", // The inner cutout for the doughnut
+        cutout: "90%",
         rotation: -85,
-        circumference: 170, // Ensures it's a semi-circle
+        circumference: 170,
       },
     ],
   };
@@ -27,10 +24,10 @@ const CarbonFootprintChart = () => {
   const options = {
     plugins: {
       legend: {
-        display: false, // This removes the default legend
+        display: false,
       },
     },
-    maintainAspectRatio: false, // Keeps the chart responsive
+    maintainAspectRatio: false,
   };
 
   return (
@@ -38,16 +35,14 @@ const CarbonFootprintChart = () => {
       <div className="chart-title">
         <h4>Your annual carbon footprint</h4>
       </div>
-      {/* Graph is styled with MUI */}
-      <Box className="chart-wrapper">
+
+      <div className="chart-wrapper">
         <Doughnut data={data} options={options} />
-        {/* "18 ton CO₂" text inside the doughnut graph */}
+
         <div className="chart-inner-text">
-          <Typography variant="h6" className="carbon-value">
-            18 ton CO₂
-          </Typography>
+          <h3 className="carbon-value">18 ton CO₂</h3>
         </div>
-      </Box>
+      </div>
 
       <div className="legend-container">
         <div className="legend-item">
@@ -63,13 +58,11 @@ const CarbonFootprintChart = () => {
           <span>Appliances</span>
         </div>
       </div>
-      
+
       <div className="message-box">
         <img src={arrow} alt="arrow" className="message-icon" />
-        {/* <span className="message-icon">📈</span> */}
-        <span className="message-text">
-          Which is 25% higher than average
-        </span>
+
+        <span className="message-text">Which is 25% higher than average</span>
       </div>
     </div>
   );
