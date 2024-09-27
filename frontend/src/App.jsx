8c707069b9d3de51ff8fb447374d55ed170f1108
MukeshR-prog <mukeshr1855@gmail.com>
2024-09-27 12:55:13 +0530

    mukesh added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Slide2 from './pages/mukesh/slide2.jsx'
+import Slide1 from './pages/mukesh/slide1.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,8 +11,8 @@ function App() {
     <>
         <BrowserRouter>
        <Routes>
-         {/* <Route path="/" element={<Home />} /> */}
-         {/* <Route path="/about" element={<About />} /> */}
+         <Route path="/" element={<Slide1 />} />
+         <Route path="/display-karma" element={<Slide2 />} />
        </Routes>
      </BrowserRouter>
     </>
